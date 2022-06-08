dbf0cfcc1c8c18f54bb1cb21b802032d1cf09c2f
AlaaShalabe <93447146+AlaaShalabe@users.noreply.github.com>
2022-06-08 11:10:45 +0300

    Create Layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Real Estate</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+</head>
+
+<body>
+    @include('partials.navbar')
+    <section class="hero">
+        <div class="hero-body">
+            <p class="title">
+                @yield('title')
+            </p>
+            <p class="subtitle">
+                @yield('subtitle')
+            </p>
+        </div>
+    </section>
+    @yield('content')
+    @include('partials.footer')
+</body>
+
+</html>
